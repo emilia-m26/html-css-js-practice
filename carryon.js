57e7adc3b156e8b6a1a770b7bc95c3e8ed60de19
Emilia <emilia.mena26@gmail.com>
2020-09-05 15:46:40 -0400

    changes color to blue, need to make functions for other button colors

--- carryon.js
+++ carryon.js
@@ -3,3 +3,7 @@ function changeText() {
     text=prompt("Type a message (e.g. Carry On):");
     document.getElementById("message").innerHTML=text;
   }
+
+  function changeColor() {
+    document.getElementById("poster").style.backgroundColor = "#3050D0";
+  }
