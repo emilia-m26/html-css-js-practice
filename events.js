0cdc31d92bad63271329d8a8a57c76f394d28e4e
Emilia <emilia.mena26@gmail.com>
2020-09-10 15:01:58 -0400

    query selector on buttons

---
+++ events.js
@@ -0,0 +1,16 @@
+console.log("it works!")
+
+const butts = document.querySelector('.butts');
+console.log(butts);
+const coolButton = document.querySelector('.cool');
+console.log(coolButton);
+
+//anonymous function directly into event listener
+// butts.addEventListener('click', function(){
+//     console.log('It got clicked!?!?');
+// })
+
+//Three Steps
+//go get something
+//listen for something
+//do something
