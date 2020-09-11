2610d2380e66f0abc3634dafc5eecfc87e984cd6
Emilia <emilia.mena26@gmail.com>
2020-09-11 17:33:25 -0400

    added image for event listener practice and some js for getting information from form

--- event-practice.js
+++ event-practice.js
@@ -1,1 +1,10 @@
 console.log('it works');
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.name.value);
+    console.log(event.currentTarget.email.value);
+    console.log(event.currentTarget.agree.checked);
+});
