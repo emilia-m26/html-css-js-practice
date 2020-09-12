c03cc619dbc570a4a9104c319c5c3028700ca840
Emilia <emilia.mena26@gmail.com>
2020-09-12 12:38:36 -0400

    selecting elements needed

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,8 +1,19 @@
 console.log('it works');
 
 //Select elements on page
+const canvas = document.querySelector('#etch-a-sketch');
+console.log(canvas);
+
+const ctx = canvas.getContext('2d');
+
+const shakeButton = document.querySelector('.shake');
+console.log(shakeButton);
+
 
 //setup canvas for drawing
+
+
+
 
 //write a draw function
 
