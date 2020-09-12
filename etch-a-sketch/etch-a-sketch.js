c5cdc0acf083f84cdb9aa97d2f5b15c2af015a95
Emilia <emilia.mena26@gmail.com>
2020-09-12 12:33:51 -0400

    planning what is needed

---
+++ etch-a-sketch/etch-a-sketch.js
@@ -0,0 +1,13 @@
+console.log('it works');
+
+//Select elements on page
+
+//setup canvas for drawing
+
+//write a draw function
+
+///write handler for keys
+
+//clear or shake function
+
+//listen for arrow keys
