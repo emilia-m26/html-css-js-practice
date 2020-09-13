6d919451f38424f6a0baf6915e76131e7ae6b199
Emilia <emilia.mena26@gmail.com>
2020-09-13 10:07:28 -0400

    set up event listener on button click

--- clickoutside-modal/click-outside.js
+++ clickoutside-modal/click-outside.js
@@ -1,1 +1,9 @@
 console.log('connected');
+
+const cardButtons =document.querySelectorAll('.card button');
+
+function handleCardButtonClick(){
+    console.log('You clicked it!');
+}
+
+cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
