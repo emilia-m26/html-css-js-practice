a9922f578a1af49583eb47677e5d92c4054d2504
Emilia <emilia.mena26@gmail.com>
2020-09-13 10:14:46 -0400

    working on handler function

--- clickoutside-modal/click-outside.js
+++ clickoutside-modal/click-outside.js
@@ -2,8 +2,17 @@ console.log('connected');
 
 const cardButtons =document.querySelectorAll('.card button');
 
-function handleCardButtonClick(){
-    console.log('You clicked it!');
-}
+//grab something to show in modal - event
+function handleCardButtonClick(event){
+    const button = event.currentTarget;
+    card = button.closest('.card');
+    console.log(card);
+    //grab image source
+    const imgSrc = card.querySelector('img').src;
+    console.log(imgSrc);
+    const desc = card.dataset.description;
+    console.log(desc);
+};
 
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
+
