fa25a00385f8f1592ee6ea08d0a813a18386e20a
Emilia <emilia.mena26@gmail.com>
2020-09-14 14:14:34 -0400

    testing different options

--- scroll-events/scroll-to-accept.js
+++ scroll-events/scroll-to-accept.js
@@ -1,13 +1,37 @@
 console.log('Connected');
-function scrollToAccept (){
-    const terms = document.querySelector('.terms-and-conditions');
-if (!terms) {
-    return; //quit this, there isn't that item on the page
+// function scrollToAccept (){
+//     const terms = document.querySelector('.terms-and-conditions');
+// if (!terms) {
+//     return; //quit this, there isn't that item on the page
+// }
+
+// terms.addEventListener('scroll', function(event){
+//     console.log(event);
+//     });
+// }
+
+// scrollToAccept();
+
+const terms = document.querySelector('.terms-and-conditions');
+const acceptButton =document.querySelector('.accept');
+
+//observer takes callback
+const observer = new IntersectionObserver(observerCallback, { 
+    root: terms, 
+    threshold: 0.5,
+});
+
+function observerCallback(payload){
+    console.log(payload[0].intersectionRatio);
 }
 
-terms.addEventListener('scroll', function(event){
-    console.log(event);
-    });
-}
+observer.observe(terms.lastElementChild);
 
-scrollToAccept();
+// terms.addEventListener('scroll', function(event){
+//     console.log(event.currentTarget.scrollTop);
+//     console.log(event.currentTarget.scrollHeight);
+// });
+
+/*use intersection observer to see if something 
+is viewable on the page*/
+
