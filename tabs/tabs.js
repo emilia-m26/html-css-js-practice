41c1f11ab12b559bd93337f98093d07847b89a58
Emilia <emilia.mena26@gmail.com>
2020-09-15 12:20:47 -0400

    setup for event listener

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -3,3 +3,9 @@ console.log(' IT WORKS!');
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(){
+    
+}
+
+tabButtons.forEach(button => button.addEventListener('click',handleTabClick));
