52270117c202d2da45d31feec429c5d8b95d1055
Emilia <emilia.mena26@gmail.com>
2020-09-15 12:23:59 -0400

    what needs to be done next

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -4,8 +4,12 @@ const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
 const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
 
-function handleTabClick(){
-    
+function handleTabClick(event){
+    //console.log(event.currentTarget);
+    //hide all tab panels
+    //mark all tabs as unselected
+    //mark the clicked tab as selected
+    //find associated tab panel and show it
 }
 
 tabButtons.forEach(button => button.addEventListener('click',handleTabClick));
