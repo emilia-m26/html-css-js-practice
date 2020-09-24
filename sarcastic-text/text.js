ee8be646cd43ac69505eacbde834c4ec402d1574
Emilia <emilia.mena26@gmail.com>
2020-09-24 15:25:57 -0400

    working on sarcastic filter

--- sarcastic-text/text.js
+++ sarcastic-text/text.js
@@ -8,11 +8,26 @@ const filterInputs = document.querySelectorAll('[name="filter"]');
 
 //handler for outputting of text
 function transformText(text) {
-    console.log(text);
+    //console.log(text);
+//take text and loop over each letter
+    const mod = Array.from(text).map(filters.sarcastic);
     result.textContent = text;
 }
 
 //listener for input event
 textArea.addEventListener('input', event => transformText(event.target.value) );
+
+//filters for different inputs
+
+const filters {
+    sarcastic(letter, index) {
+        if (index % 2) {
+            return letter.toUpperCase();
+        }
+        return letter.toLowerCase();
+    },
+    funky() {},
+    unable() {},
+}
 
 
