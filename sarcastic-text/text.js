63a5d328b812d905e3dcc93b814c058e811d02c8
Emilia <emilia.mena26@gmail.com>
2020-09-24 15:30:02 -0400

    sarcastic filter working

--- sarcastic-text/text.js
+++ sarcastic-text/text.js
@@ -6,20 +6,9 @@ const filterInputs = document.querySelectorAll('[name="filter"]');
 //console.log(result);
 //console.log(filterInputs);
 
-//handler for outputting of text
-function transformText(text) {
-    //console.log(text);
-//take text and loop over each letter
-    const mod = Array.from(text).map(filters.sarcastic);
-    result.textContent = text;
-}
-
-//listener for input event
-textArea.addEventListener('input', event => transformText(event.target.value) );
-
 //filters for different inputs
 
-const filters {
+const filters = {
     sarcastic(letter, index) {
         if (index % 2) {
             return letter.toUpperCase();
@@ -30,4 +19,16 @@ const filters {
     unable() {},
 }
 
+
+//handler for outputting of text
+function transformText(text) {
+    //console.log(text);
+//take text and loop over each letter
+    const mod = Array.from(text).map(filters.sarcastic);
+    console.log(mod);
+    result.textContent = mod.join('');
+}
+
+//listener for input event
+textArea.addEventListener('input', event => transformText(event.target.value) );
 
