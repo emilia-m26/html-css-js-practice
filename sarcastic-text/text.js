e615316e0ecb48d1e12d7f820578a72e821dbfe5
Emilia <emilia.mena26@gmail.com>
2020-09-24 15:05:15 -0400

    selecting items needed:

---
+++ sarcastic-text/text.js
@@ -0,0 +1,7 @@
+const textArea = document.querySelector('[name="text"]');
+const result = document.querySelector('.result');
+const filterInputs = document.querySelectorAll('[name="filter"]');
+
+console.log(textArea);
+console.log(result);
+console.log(filterInputs);
