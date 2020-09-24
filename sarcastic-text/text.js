34e5fa860e146ce6540cdeb9099b4a5cfb7855cb
Emilia <emilia.mena26@gmail.com>
2020-09-24 15:16:11 -0400

    handler and listener

--- sarcastic-text/text.js
+++ sarcastic-text/text.js
@@ -2,6 +2,17 @@ const textArea = document.querySelector('[name="text"]');
 const result = document.querySelector('.result');
 const filterInputs = document.querySelectorAll('[name="filter"]');
 
-console.log(textArea);
-console.log(result);
-console.log(filterInputs);
+//console.log(textArea);
+//console.log(result);
+//console.log(filterInputs);
+
+//handler for outputting of text
+function transformText(text) {
+    console.log(text);
+    result.textContent = text;
+}
+
+//listener for input event
+textArea.addEventListener('input', event => transformText(event.target.value) );
+
+
