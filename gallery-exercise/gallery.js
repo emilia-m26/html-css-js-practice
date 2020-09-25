5c7188cc552de6f81b52f8a71d767c116a2237b7
Emilia <emilia.mena26@gmail.com>
2020-09-25 10:51:47 -0400

    variables to select images and buttons needed

--- gallery-exercise/gallery.js
+++ gallery-exercise/gallery.js
@@ -3,7 +3,16 @@ console.log('it works');
 /*create a closure so separate galleries will not 
 intefere with each other but reuse same code */
 function Gallery(gallery) {
-    console.log(gallery);
+    //console.log(gallery);
+    if (!gallery) {
+        throw new Error('no gallery found');
+    }
+//select elements we need
+    const images = Array.from(gallery.querySelectorAll('img'));
+    //console.log(images);
+    const modal = document.querySelector('.modal');
+    const prevButton = modal.querySelector('.prev');
+    const nextButton = modal.querySelector('.next');
 }
 
 const gallery1 = Gallery(document.querySelector('.gallery1'));
