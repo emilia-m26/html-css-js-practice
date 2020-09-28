407633a55439c5ac823b2ee3561c7a3e0b6c52c3
Emilia <emilia.mena26@gmail.com>
2020-09-28 12:58:17 -0400

    reversing a string, attempt 1

--- practice.js
+++ practice.js
@@ -1,2 +1,9 @@
 console.log('connected');
 
+let string = "cool";
+
+function reverseString(string) {
+   let arrayFromString = Array.from(string);
+   arrayFromString = arrayFromString.reverse().join('');
+   return arrayFromString;
+}
