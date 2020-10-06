e2548ff084ba9b66a95ebb3bc1bacabf6b5619f2
Emilia <emilia.mena26@gmail.com>
2020-10-06 13:16:33 -0400

    pop with fields

--- async-prompts/scripts.js
+++ async-prompts/scripts.js
@@ -4,7 +4,15 @@ console.log('connected');
 function ask(options) {
     return new Promise (function(resolve) {
 //need to create a popup with fields in it
+    const popup = document.createElement('form');
+    popup.classList.add('popup');
+    popup.insertAdjacentHTML('afterbegin', `
+     <fieldset>
+     <label>${options.title}</label>
+     </fieldset>
+    `);
 
+    //console.log(popup);
 //check if they want cancel button
 
 //listen for submit event on inputs
