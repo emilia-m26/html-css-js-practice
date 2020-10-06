f1cef28b46f3701882fe81d9f69b3bab169908b0
Emilia <emilia.mena26@gmail.com>
2020-10-06 13:04:56 -0400

    comments organizing what needs to be done

---
+++ async-prompts/scripts.js
@@ -0,0 +1,15 @@
+console.log('connected');
+
+//make prompt function
+function ask(options) {
+    return new Promise (function(resolve) {
+//need to create a popup with fields in it
+
+//check if they want cancel button
+
+//listen for submit event on inputs
+
+//when submitted, resolve data that was in inpput field
+
+    });
+}
