9a84e5bb5872f18f3bb466f4efa1174188daf53c
Emilia <emilia.mena26@gmail.com>
2020-10-12 12:32:46 -0400

    random number function

--- async-typer/scripts.js
+++ async-typer/scripts.js
@@ -1,3 +1,7 @@
 function wait(ms = 0) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()){
+    return Math.floor(randomNumber * (max - min) + min);
+}
