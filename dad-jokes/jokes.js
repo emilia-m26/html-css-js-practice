ff7c470203b5c2ba2c7f39f35e0e60f84bc5eee0
Emilia <emilia.mena26@gmail.com>
2020-10-13 17:48:03 -0400

    utility function to use random text on joke button

--- dad-jokes/jokes.js
+++ dad-jokes/jokes.js
@@ -23,10 +23,21 @@ async function fetchJoke() {
   return data;
 }
 
+//utility function
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if(item === not) {
+    //recursion
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
 async function handleClick() {
   const { joke } = await fetchJoke();
   //console.log(joke);
   jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
 }
 
 jokeButton.addEventListener('click', handleClick);
